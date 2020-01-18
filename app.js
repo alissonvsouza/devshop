2ee088e21125eead2bb19346231b8ab8c0c92e1b
Alisson Souza <alissonvsouza8@gmail.com>
2020-01-18 15:55:20 -0200

    Add body-parser and express-session modules

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const init = db => {
     const category = require('./models/category')
     const routes = require('./routes')
 
+    const bodyParser = require('body-parser')
+    const session = require('express-session')
+
+    app.use(bodyParser.json({ extended: true }))
+    app.use(bodyParser.urlencoded())
+
+    app.use(session({
+        secret: 'MyShopSessionRules!',
+        name: 'sessionId'
+    }))
+
     app.set('view engine', 'ejs')
     app.use(express.static('public'))
 
     app.use(async (req, res, next) => {
         const categories = await category.getCategories(db)()
+        const { user } = req.session
         res.locals = {
-            categories
+            categories,
+            user
         }
         next()
     })
