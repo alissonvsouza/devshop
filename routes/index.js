c713cd654b3c33c98332afeb4ca9041627b8c5be
Alisson Souza <alissonvsouza8@gmail.com>
2020-01-23 21:11:40 -0200

    Add admin's router

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ const init = db => {
     const auth = require('../controllers/auth')
     const categories = require('./categories')
     const products = require('./products')
+    const admin = require('./admin')
     
     const router = require('express').Router()
 
     router.get('/', home.getIndex)
     router.post('/login', auth.login(db))
     router.get('/logout', auth.logout)
+
+    router.use('/admin', admin(db))
     router.use('/categories', categories(db))
     router.use('/products', products(db))
 
