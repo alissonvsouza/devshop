21fcc061960b55a81f709e636985587003fd4a0b
Alisson Souza <alissonvsouza8@gmail.com>
2020-01-25 14:26:08 -0200

    categories' router updated

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -1,7 +1,11 @@
 const init = db => {
     const router = require('express').Router()
     const categories = require('../../controllers/categories')
+
     router.get('/', categories.adminGetCategories(db))
+    router.get('/new', categories.adminCreateCategory(db))
+    router.post('/new', categories.adminCreateCategory(db))
+
     return router
 }
 module.exports = init
